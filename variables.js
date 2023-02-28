c43f13e10cd6570e44ca6bd784dfb492ca64fad2
Marisa Wyatt <risawyatt22@gmail.com>
2023-02-28 15:46:01 -0700

    Reassign vars, run log statements

--- variables.js
+++ variables.js
@@ -1,9 +1,9 @@
-var name = "Marisa"
-
+var firstName = "Marisa"
+firstName = "Mars"
 var dog = "Smokey"
-
+dog = "Pokers"
 var email = "risawyatt22@gmail.com"
-
+email = "mars5280@gmail.com"
 var age = 26
 
 var rent = 1095
@@ -13,3 +13,9 @@ var hours = 40
 var focus = true
 
 var graded = false
+
+console.log(firstName)
+
+console.log(age)
+
+console.log(focus)
